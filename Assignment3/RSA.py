aa7b0e9f4875b90c4e9d7b6bcacdb467ed8e8a40
froydis98 <44896043+froydis98@users.noreply.github.com>
2020-10-25 17:26:40 +0100

    Encrypt and decrypt small strings RSA

--- Assignment3/RSA.py
+++ Assignment3/RSA.py
@@ -1,3 +1,7 @@
+from Crypto.Util.number import bytes_to_long, long_to_bytes
+from Crypto.Random import get_random_bytes 
+import Crypto
+
 def totient(p, q):
     return (p-1) * (q-1)
 
@@ -33,22 +37,63 @@ def generateKeys(p, q, n):
     d = modinv(e, phi)
     return phi, e, d
 
-def encrypt(message, e, n):
-    return message**e % n
+def egcd(e,phi):
+    while(phi!=0):
+        e,phi=phi,e%phi
+    return e
 
-def decrypt(cipher, d, n):
-    return cipher**d % n
+#Extended Euclidean Algorithm
+def eea(a,b):
+    if(a%b==0):
+        return(b,0,1)
+    else:
+        gcd,s,t = eea(b,a%b)
+        s = s-((a//b) * t)
+        return(gcd,t,s)
+ 
+#Multiplicative Inverse
+def mult_inv(e,phi):
+    gcd,s,_=eea(e,phi)
+    if(gcd!=1):
+        print('The modular inverse does not exist')
+        return None
+    else:
+        if(s<0):
+            print("s=%d. Since %d is less than 0, s = s(modr), i.e., s=%d."%(s,s,s%phi))
+        elif(s>0):
+            print("s=%d."%(s))
+        return s%phi
 
 def main():
-    p = 11
-    q = 13
+    # Decide how many bits we want our primes to be
+    bits = 60
+    p = Crypto.Util.number.getPrime(bits, randfunc=get_random_bytes)
+    q = Crypto.Util.number.getPrime(bits, randfunc=get_random_bytes)
     n = p*q
-    phi, e, d = generateKeys(p, q, n)
-    message = 9
-    cipher = encrypt(message, e, n)
-    decrypted = decrypt(cipher, d, n)
-    print(decrypted)
+    message = 'heihei'
+    print('The message is:', message)
+    phi = totient(p, q)
+    for i in range(1,1000):
+        if(egcd(i,phi)==1):
+            e=i
+    d = mult_inv(e, phi)
+    m = bytes_to_long(message.encode('utf-8'))
+    print("p =", p)
+    print("q =", q)
+    print("e =", e)
+    print("d =", d)
+    # Encrypt bytes
+    cipher = pow(m,e, n)
+    print("The cipher is:", cipher)
+    # Decrypt bytes
+    decipher = pow(cipher,d ,n)
+    plaintext = long_to_bytes(decipher)
+    print("The plaintext after decrypting the cipher: ", plaintext)
+    
 
 if __name__ == "__main__":
     main()
-    
+
+""" p = Crypto.Util.number.getPrime(bits, randfunc=get_random_bytes)
+q = Crypto.Util.number.getPrime(bits, randfunc=get_random_bytes)
+ """
